f2448bc5340853c62286eec6303c6d6fa13fdec4
Lukas <Lukas.schoelch@gmail.com>
2025-04-17 09:55:36 +0200

    kBar example impl.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,163 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { KBarProvider, useKBar } from 'kbar'
+import CommandBar from './assets/components/ui/af_kbar'
+
+import {ChartSpline, Play, CircleGauge, List, ZoomIn, ZoomOut, 
+  SwatchBook, Contrast, Sun, Moon,
+  ChartArea, FileChartLine, Grid3X3,  } from "lucide-react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const actions = [
+    {
+      id: "quick-options",
+      name: "Quick Options",
+      shortcut: ["q"],
+      keywords: "quick, quickoptions",
+      // perform: () => {},
+      icon: <List className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "switch-function",
+      name: "Switch Function",
+      // shortcut: ["s"],
+      keywords: "switch, switch function",
+      parent: "quick-options",
+      perform: () => {},
+      icon: <ChartSpline  className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "play-audio",
+      name: "Play Audio",
+      // shortcut: [""],
+      // keywords: ", ",
+      parent: "quick-options",
+      perform: () => {},
+      icon: <Play className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "change-audio-speed",
+      name: "Change Audio Speed",
+      // shortcut: [""],
+      // keywords: ", ",
+      parent: "quick-options",
+      perform: () => {},
+      icon: <CircleGauge className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "zoom-in",
+      name: "Zoom In",
+      // shortcut: [""],
+      // keywords: ", ",
+      parent: "quick-options",
+      perform: () => {},
+      icon: <ZoomIn className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "zoom-out",
+      name: "Zoom Out",
+      // shortcut: [""],
+      // keywords: ", ",
+      parent: "quick-options",
+      perform: () => {},
+      icon: <ZoomOut className="size-5 shrink-0 opacity-70" />,
+    },
+
+
+    {
+      id: "diagram-options",
+      name: "Diagram Options",
+      // shortcut: [""],
+      keywords: "diagramm",
+      // perform: () => {},
+      icon: <FileChartLine className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "toggle-grid",
+      name: "Toggle Grid",
+      // shortcut: [""],
+      keywords: "grid",
+      parent: "diagram-options",
+      perform: () => {},
+      icon: <Grid3X3 className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "set-view",
+      name: "Set View",
+      // shortcut: [""],
+      keywords: "view",
+      parent: "diagram-options",
+      perform: () => {},
+      icon: <ChartArea className="size-5 shrink-0 opacity-70" />,
+    },
+
+
+
+
+    {
+      id: "change-function",
+      name: "edit functions",
+      shortcut: ["f"],
+      keywords: "function, change function, change graph, graph, edit function, edit graph",
+      //  section: "",
+      perform: () => {},
+      icon: <ChartSpline className="size-5 shrink-0 opacity-70" />,
+    },
+
+
+    {
+      id: "change-theme",
+      name: "Change Theme",
+      // shortcut: [""],
+      keywords: "theme",
+      // perform: () => {},
+      icon: <SwatchBook className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "light-theme",
+      name: "Light Theme",
+      // shortcut: [""],
+      keywords: "theme",
+      parent: "change-theme",
+      perform: () => {},
+      icon: <Sun className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "dark-theme",
+      name: "Dark Theme",
+      // shortcut: [""],
+      keywords: "theme",
+      parent: "change-theme",
+      perform: () => {},
+      icon: <Moon className="size-5 shrink-0 opacity-70" />,
+    },
+    {
+      id: "high-contrast-theme",
+      name: "High Contrast Theme",
+      // shortcut: [""],
+      keywords: "theme",
+      parent: "change-theme",
+      perform: () => {},
+      icon: <Contrast className="size-5 shrink-0 opacity-70" />,
+    },
+
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <KBarProvider actions={actions}>
+        <CommandBar/>
+        <h1 className='text-3xl font-bold underline'>Under Construction</h1>
+        {/* <OpenCommandBarButton /> */}
+      </KBarProvider>
     </>
   )
+}
+
+
+const OpenCommandBarButton = () => {
+  const { query } = useKBar();
+  return <button onClick={query.toggle}>Open CommandBar</button>;
 }
 
 export default App
---
+++ src/assets/components/ui/af_kbar.jsx
@@ -0,0 +1,154 @@
+import { KBarAnimator, KBarPortal, KBarPositioner, KBarResults, KBarSearch, useMatches, useKBar } from 'kbar';
+import React, { useEffect, useRef, useState } from 'react';
+
+const CustomDialogAnimator = ({ children, className }) => {
+    // needed, to trap the keyboard focus - kbar doesn't do this by default
+
+    const dialogRef = useRef(null);
+  
+    useEffect(() => {
+      const dialog = dialogRef.current;
+  
+      if (dialog) {
+        dialog.showModal();
+      }
+  
+      return () => {
+        if (dialog && dialog.open) {
+          dialog.close();
+        }
+      };
+    }, []);
+  
+    return (
+      <dialog
+        role='dialog'                // needed for my aria-checker browser plugin
+        aria-label="command palette"
+        aria-modal='true'
+        ref={dialogRef}
+        className={`${className}`}
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        {children}
+      </dialog>
+    );
+  };
+
+
+const CommandBar = () => {
+    return (
+        <KBarPortal>
+            <KBarPositioner className="bg-black/50 backdrop-blur-md">
+                <CustomDialogAnimator className='bg-white rounded-xl shadow-xl flex flex-col gap-4 w-[35rem] overflow-hidden'>
+                    <KBarAnimator>
+                        <KBarSearch className='w-full outline-none px-6 py-4 text-black' aria-owns='kbar-listbox'/>
+                        <SearchResults aria-hi/>
+                    </KBarAnimator>
+                    <LiveRegion />
+                </CustomDialogAnimator>
+            </KBarPositioner>
+        </KBarPortal>
+    );
+};
+
+const SearchResults = () => {
+    const { results } = useMatches();
+
+    return (
+        <KBarResults 
+            items={results}
+            onRender={({ item, active }) => {
+                return typeof item === 'string' ? (
+                    // Section header
+                    <div className='text-xm uppercase px-4 pt-3 pb-1 text-neutral-500 font-bold'
+                    role="separator"
+                    aria-hidden="true"
+                    >
+                        {item}
+                    </div>
+                ) : (
+                    // Single action
+                    <div 
+                        className={`text-black flex items-center px-4 py-3 ${active ? "bg-[#eeeeee]" : "bg-transparent"}`}
+                        tabIndex={0}
+                        role="option"
+                    >
+                        {item.icon && <span className="mr-2">{item.icon}</span>}
+                        <div className="flex flex-col">
+                            <span>{item.name}</span>
+                            {item.subtitle && <span className="text-sm text-gray-500">{item.subtitle}</span>}
+                        </div>
+                        {item.shortcut && (
+                            <span className="ml-auto text-xs text-gray-400" aria-label={`Shortcut: ${item.shortcut.join(", ")}`}>
+                                {item.shortcut.join(", ")}
+                            </span>
+                        )}
+                    </div>
+                );
+            }}
+        />
+    )
+}
+
+// LiveRegion for opt screenreader announcements
+const LiveRegion = () => {
+    const { results } = useMatches();
+    const [announcement, setAnnouncement] = useState("");
+    const { query, options } = useKBar();
+    const prevMessage = useRef("init");
+    const timeoutRef = useRef(null);
+
+    useEffect(() => {
+        const firstResult = results.find((item) => typeof item !== "string");
+        const message = firstResult 
+            ? `${firstResult.name}` 
+            : "Keine Ergebnisse gefunden";
+    
+        // console.log("prev: ", prevMessage.current);
+        // console.log("message: ", message);
+        // console.log("index: ", results.indexOf(firstResult));
+        if (message === prevMessage.current) {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current); // stop pending setAnnouncement calls
+            }
+
+            // console.log("read");
+            setAnnouncement(""); // clear the announcement - will also trigger a screen reader announcement if content doesn't change
+            timeoutRef.current = setTimeout(() => setAnnouncement(message), 500); // set content after a short delay
+        }
+        else if (results.indexOf(firstResult) === 0) {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current); // stop pending setAnnouncement calls
+            }
+
+            // console.log("subMenu Helper");
+            setAnnouncement(message);
+        } 
+        else {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current); // stop pending setAnnouncement calls
+            }
+            setAnnouncement("");
+        }
+        // console.log("set to:", message);
+        prevMessage.current = message;
+
+    }, [results, query.query]);
+
+    return (
+        <div
+            aria-live="assertive"
+            aria-atomic="true"
+            className="sr-only"
+        >
+            {announcement}
+        </div>
+    );
+}
+
+export default CommandBar;
