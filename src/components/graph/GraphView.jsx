e7d796b947d95acb0494bed5b477bddfc0a40d6b
pedritomelenas <pedro@ugr.es>
2025-05-01 20:20:26 +0200

    snapping added

--- src/components/graph/GraphView.jsx
+++ src/components/graph/GraphView.jsx
@@ -6,15 +6,15 @@ import { create, all } from 'mathjs'
 const config = { }
 const math = create(all, config)
 
-var xisolated = [];
-
-
 // txtraw is a string of the form [[expr_1,ineq_1],[expr_2,ineq_2],..,[expr_n,ineq_n]]
 // where expr_i is an expression in the variable x that defines a function in the interval defined by ineq_i
 // for instance [[x+5,x < -4],[x^2,-4<=x < 1],[x-2,1<=x < 3],[5,x==3],[x-2,3 < x < 5],[3,5<= x]]
 
 function parsePiecewise(txtraw){
     var parsed = math.parse(txtraw);
+    if (!("items" in parsed)){ // not a piecewise function
+      return txtraw;
+    }
     var l = parsed.items;
     var v,a,b,i;
     var result= "";
@@ -42,9 +42,13 @@ function parsePiecewise(txtraw){
 
 function createEndPoints(txtraw,board){
     var parsed = math.parse(txtraw);
+    if (!("items" in parsed)){ // not a piecewise function
+        return [[],[]];
+    }
     var l = parsed.items;
     var ineq,v,a,b,p,i;
-    var endpoints = [];
+    const endpoints = [];
+    const xisolated = [];
     for (i=0;i< l.length;i++){
         ineq = l[i].items[1];
         if ("op" in ineq){ //that is a single inequality or an equality
@@ -55,6 +59,7 @@ function createEndPoints(txtraw,board){
                     endpoints.push(p);
                     if (ineq.op == "=="){
                         xisolated.push(p.X());
+                        console.log("found isolated point at ",p.X());
                     }
                 }else{ // we have x op a, with op in {<=, >=, ==}
                     v=ineq.args[1].evaluate();
@@ -62,6 +67,7 @@ function createEndPoints(txtraw,board){
                     endpoints.push(p);
                     if (ineq.op == "=="){
                         xisolated.push(p.X());
+                        console.log("found isolated point at ",p.X());
                     }
                 }
             }
@@ -97,13 +103,16 @@ function createEndPoints(txtraw,board){
             }
         }
     }
-    return endpoints;
+    return [endpoints,xisolated];
 }
 
 
 const GraphView = () => {
   const boardRef = useRef(null);
   const { functionInput, setCursorCoords, setInputErrorMes, graphBounds } = useGraphContext();
+  var endpoints = [];
+  var xisolated = [];
+  var snapaccuracy;
 
   useEffect(() => {
     const board = JXG.JSXGraph.initBoard("jxgbox", {
@@ -115,7 +124,7 @@ const GraphView = () => {
     });
 
     boardRef.current = board;
-
+    snapaccuracy = 3/board.unitX;
 
     let graphFormula;
     try {
@@ -129,27 +138,36 @@ const GraphView = () => {
       graphFormula = 0;
     }
 
-    const graphObject = board.create("functiongraph", [graphFormula]);
+    const graphObject = board.create("functiongraph", [graphFormula],{fixed:true,highlight:false});
+
+    if (graphFormula != 0){
+      [endpoints,xisolated] = createEndPoints(functionInput, board);
+      console.log("endpoints", endpoints);
+      console.log("xisolated", xisolated[0]);
+    }else{
+      endpoints = [];
+      xisolated = [];
+    }
     const cursor = board.create("point", [0, 0], {
       name: "",
       size: 3,
       color: "red",
       fixed: true,
+      highlight: false
     });
-
-    if (graphFormula != 0){
-      const endpoints = createEndPoints(functionInput, board);
-      console.log("endpoints", endpoints);
-      console.log("xisolated", xisolated);
-    }else{
-      const endpoints = [];
-    }
 
     const updateCursor = (event) => {
       const coords = board.getUsrCoordsOfMouse(event);
-      const x = coords[0];
+      var x = coords[0];
+      const l=xisolated.filter(function(e){return Math.abs(e-x)<snapaccuracy});
+      if (l.length>0){
+        console.log("isolated point");
+        x=l[0];
+        console.log("x",x);
+        console.log("y",graphFormula(x)+1);
+      }
       const y = graphFormula(x);
-      cursor.setPosition(JXG.COORDS_BY_USER, [x, y]);
+      cursor.setPositionDirectly(JXG.COORDS_BY_USER, [x, y]);
       setCursorCoords({ x: x.toFixed(2), y: y.toFixed(2) });
       board.update();
     };
